f41b3f1b6688312e6b83b87d77ab491a7a81601f
Nara <nara773.work@gmail.com>
2026-06-16 15:25:44 +0900

    Add ER.drawio

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+use App\Http\Controllers\BookController;
+Route::get('/books',[BookController::class,'index']);
+Route::get('/books/{$book->id}',[BookController::class,'show']);
+Route::get('/books',[BookController::class,'store'])->name('books.create');
+Route::get('/books/{$book->id}/edit',[BookController::class,'edit'])->name('books.create');
